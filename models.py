19b1b2c52ef7fc2c0e6936b99c6b2bd597c40475
sasha wangui <sasha.iluku@gmail.com>
2023-09-08 02:16:47 +0300

    table fix?

--- models.py
+++ models.py
@@ -1,16 +1,16 @@
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, create_engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql import func
 
+engine = create_engine('sqlite:///expenses.db')
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    email = Column(String(50))
+    username = Column(String, unique=True, nullable=False)
 
     expenses = relationship('Expense', back_populates='user')
 
@@ -26,10 +26,12 @@ class Expense(Base):
     __tablename__ = 'expenses'
 
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
-    category_id = Column(Integer, ForeignKey('categories.id'))
-    amount = Column(Integer)
-    date = Column(DateTime, default=func.now())
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    category = Column(String, nullable=False)
+    amount = Column(Integer, nullable=False)
+    date = Column(Date, nullable=False, default=func.now())
 
     user = relationship('User', back_populates='expenses')
-    category = relationship('Category', back_populates='expenses')
+    category = relationship ("Category", back_populates='expenses')
+
+Base.metadata.create_all(engine)
--- tables.py
+++ tables.py
@@ -1,6 +1,34 @@
-from sqlalchemy import create_engine
-from models import Base
+from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+from models import User, Expense
 
-DB_URL = 'sqlite:///expenses.db'
-engine = create_engine(DB_URL)
+# Create an SQLite database engine
+engine = create_engine('sqlite:///expenses.db')
+
+# Create a base class for declarative models
+Base = declarative_base()
+
+# Seed some sample data
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create users
+user1 = User(username='john_doe')
+user2 = User(username='jane_smith')
+
+# Add users to the session
+session.add_all([user1, user2])
+session.commit()
+
+# Create expenses
+expense1 = Expense(user=user1, category='Food', amount=250, date='2023-09-03')
+expense2 = Expense(user=user1, category='Transport', amount=50, date='2023-09-04')
+expense3 = Expense(user=user2, category='Entertainment', amount=100, date='2023-09-03')
+
+# Add expenses to the session
+session.add_all([expense1, expense2, expense3])
+session.commit()
+
 Base.metadata.create_all(engine)
